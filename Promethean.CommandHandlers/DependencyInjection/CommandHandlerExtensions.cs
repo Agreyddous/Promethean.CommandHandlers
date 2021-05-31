bcf6bf852c94dde1ffa62ac481de8d3ca0c25e37
Nando <fernandovbmgomes@hotmail.com>
2021-05-31 16:08:26 -0300

    Union local and referencend assemblies

--- Promethean.CommandHandlers/DependencyInjection/CommandHandlerExtensions.cs
+++ Promethean.CommandHandlers/DependencyInjection/CommandHandlerExtensions.cs
@@ -12,7 +12,7 @@ namespace Promethean.CommandHandlers.DependencyInjection
 		{
 			serviceCollection.AddScoped<IHandler, Handler>();
 
-			foreach (Type existingType in Assembly.GetEntryAssembly().GetReferencedAssemblies().Select(Assembly.Load).SelectMany(assembly => assembly.DefinedTypes))
+			foreach (Type existingType in AppDomain.CurrentDomain.GetAssemblies().Union(Assembly.GetEntryAssembly().GetReferencedAssemblies().Select(Assembly.Load)).SelectMany(assembly => assembly.GetTypes()))
 				foreach (Type implementedCommandHandler in existingType.GetInterfaces().Where(implementedInterface => implementedInterface.IsGenericType && (implementedInterface.GetGenericTypeDefinition().Equals(typeof(ICommandHandler<,>)) || implementedInterface.GetGenericTypeDefinition().Equals(typeof(IAsyncCommandHandler<,>)))))
 					serviceCollection.AddScoped(implementedCommandHandler, existingType);
 
