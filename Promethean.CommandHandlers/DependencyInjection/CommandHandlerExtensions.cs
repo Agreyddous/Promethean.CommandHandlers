a27046e6a04ee82134899f2d22c6d6ba13290973
Nando <fernandovbmgomes@hotmail.com>
2021-05-31 15:53:36 -0300

    Loading all referenced assemblies

--- Promethean.CommandHandlers/DependencyInjection/CommandHandlerExtensions.cs
+++ Promethean.CommandHandlers/DependencyInjection/CommandHandlerExtensions.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
 using Promethean.CommandHandlers.Handlers;
+using System.Reflection;
 
 namespace Promethean.CommandHandlers.DependencyInjection
 {
@@ -11,7 +12,7 @@ namespace Promethean.CommandHandlers.DependencyInjection
 		{
 			serviceCollection.AddScoped<IHandler, Handler>();
 
-			foreach (Type existingType in AppDomain.CurrentDomain.GetAssemblies().SelectMany(assembly => assembly.GetTypes()))
+			foreach (Type existingType in Assembly.GetEntryAssembly().GetReferencedAssemblies().Select(Assembly.Load).SelectMany(assembly => assembly.DefinedTypes))
 				foreach (Type implementedCommandHandler in existingType.GetInterfaces().Where(implementedInterface => implementedInterface.IsGenericType && (implementedInterface.GetGenericTypeDefinition().Equals(typeof(ICommandHandler<,>)) || implementedInterface.GetGenericTypeDefinition().Equals(typeof(IAsyncCommandHandler<,>)))))
 					serviceCollection.AddScoped(implementedCommandHandler, existingType);
 
